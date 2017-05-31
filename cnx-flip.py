3352759ff8189ccd49eafd4db951520cf06f23fc
tianlan95 <tl47@rice.edu>
2017-05-31 16:24:33 -0500

    use SQLAlchemy

---
+++ cnx-flip.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+from flask.ext.sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+
+# This can save the schema part
+
+# Change this config uri later!
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/pre-registration'
+db = SQLAlchemy(app)
+
+# Create our database model
+class Deck(db.Model):
+    _tablename = "decks"
+    deck_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique=True)
+
+    def __init__(self, title):
+        title.email = title
+
+    def __repr__(self):
+        return '<Title %r>' % self.title
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/createDeck', methods=['POST'])
+def createDeck():
+    if request.method == 'POST':
+        deck_title = request.form['title']
+        # Check if the title dalready exist
+        if not db.session.query(Deck).filter(Deck.title == title).count() < 1:
+            new_deck = Deck(title)
+            db.session.add(new_deck)
+            db.session.commit()
+            
+            # change the template name later!!
+            return render_template('success_create_new_deck.html')
+    return render_template('index.html')
