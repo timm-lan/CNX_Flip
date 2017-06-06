24ae7fee17082e9efa4bda85243190f6ea49ab10
tianlan95 <tl47@rice.edu>
2017-06-06 14:53:54 -0500

    update template

--- cnx_flip/views.py
+++ cnx_flip/views.py
@@ -1,6 +1,18 @@
 from pyramid.view import view_config
 
-
-@view_config(route_name='home', renderer='templates/mytemplate.jinja2')
+@view_config(route_name='home', renderer = 'templates/index.html.jinja2')
 def my_view(request):
     return {'project': 'cnx_flip'}
+
+@view_config(route_name='add_card', renderer = 'templates/success.html.jinja2')
+def success_add_card_view(request):
+    return {'project': 'cnx_flip'}
+
+
+# @view_config(route_name='generate_ajax_data', renderer='json')
+# def my_ajax_view(request):
+#     return {'message': "Hello"}
+# 
+# @view_config(route_name='home', renderer='templates/mytemplate.jinja2')
+# def my_view(request):
+#     return {'project': 'cnx_flip'}
