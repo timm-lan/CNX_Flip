139f32b916105cf0c9f4cc51a535da6c5b9449c3
tianlan95 <tl47@rice.edu>
2017-06-13 14:34:33 -0500

    update to pyramid

--- cnx_flip/__init__.py
+++ cnx_flip/__init__.py
@@ -1,12 +1,25 @@
 from pyramid.config import Configurator
-
+#DK how to use engine_from_config
+from sqlalchemy import engine_from_config, create_engine
+from .models import DBSession, Base
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+#     engine = engine_from_config(settings, 'sqlalchemy.')
+
+    engine = create_engine('postgresql+psycopg2://Tim:Qasdew123@localhost/flashcarddb')
+
+    DBSession.configure(bind=engine)
+    Base.metadata.bind = engine
+
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
+    config.include('pyramid_mako')
+    config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+#     config.add_route('generate_ajax_data', '/ajax_view')
+    config.add_route('add_card', '/addCard')
     config.scan()
     return config.make_wsgi_app()
--- cnx_flip/views.py
+++ cnx_flip/views.py
@@ -1,4 +1,6 @@
 from pyramid.view import view_config
+from .models import DBSession, Card, Base
+import transaction
 
 @view_config(route_name='home', renderer = 'templates/index.html.jinja2')
 def my_view(request):
@@ -6,7 +8,14 @@ def my_view(request):
 
 @view_config(route_name='add_card', renderer = 'templates/success.html.jinja2')
 def success_add_card_view(request):
+    req_post = request.POST
+    term = str(req_post['term'])
+    definition = str(req_post['definition'])
+    with transaction.manager:
+        model = Card(term=term, definition=definition)
+        DBSession.add(model)
     return {'project': 'cnx_flip'}
+
 
 
 # @view_config(route_name='generate_ajax_data', renderer='json')
